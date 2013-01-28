b3e63d543a378f93b82ed11943a1807181859785
Claus Hoefele <claus@claushoefele.com>
2013-01-28 21:20:19 +0100

    Calculate table view height taking keyboard into account

--- Stolpersteine/SearchDisplayController.m
+++ Stolpersteine/SearchDisplayController.m
@@ -27,12 +27,36 @@
         self.searchBar = searchBar;
         self.viewController = viewController;
         
-        CGRect frame = CGRectMake(0, 0, viewController.view.frame.size.width, viewController.view.frame.size.height - searchBar.frame.size.height);
+        CGRect frame = CGRectMake(0, 0, viewController.view.frame.size.width, viewController.view.frame.size.height);
         self.tableView = [[UITableView alloc] initWithFrame:frame style:UITableViewStylePlain];
         self.tableView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+        
+        [NSNotificationCenter.defaultCenter addObserver:self selector:@selector(keyboardDidShow:) name:UIKeyboardDidShowNotification object:nil];
+        [NSNotificationCenter.defaultCenter addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
     }
     
     return self;
+}
+
+- (void)dealloc
+{
+    [NSNotificationCenter.defaultCenter removeObserver:self];
+}
+
+- (void)keyboardDidShow:(NSNotification *)notification
+{
+    NSValue *keyboardFrameAsValue = [notification.userInfo objectForKey:UIKeyboardFrameBeginUserInfoKey];
+    CGRect keyboardFrame = [self.viewController.view convertRect:keyboardFrameAsValue.CGRectValue toView:nil];
+    CGRect frame = self.tableView.frame;
+    frame.size.height = self.viewController.view.frame.size.height - keyboardFrame.size.height;
+    self.tableView.frame = frame;
+}
+
+- (void)keyboardWillHide:(NSNotification *)notification
+{
+    CGRect frame = self.tableView.frame;
+    frame.size.height = self.viewController.view.frame.size.height;
+    self.tableView.frame = frame;
 }
 
 - (void)setActive:(BOOL)active animated:(BOOL)animated

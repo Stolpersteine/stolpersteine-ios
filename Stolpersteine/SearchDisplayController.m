82ee2bab4d3141c24c40aed620c019ff2921df94
Claus Hoefele <claus@claushoefele.com>
2013-01-28 21:43:06 +0100

    Animation to show table view

--- Stolpersteine/SearchDisplayController.m
+++ Stolpersteine/SearchDisplayController.m
@@ -27,11 +27,13 @@
         self.searchBar = searchBar;
         self.viewController = viewController;
         
-        CGRect frame = CGRectMake(0, 0, viewController.view.frame.size.width, viewController.view.frame.size.height);
+        CGRect frame = CGRectMake(0, 0, self.viewController.view.frame.size.width, self.viewController.view.frame.size.height);
         self.tableView = [[UITableView alloc] initWithFrame:frame style:UITableViewStylePlain];
         self.tableView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+        self.tableView.hidden = TRUE;
+        self.tableView.alpha = 0;
         
-        [NSNotificationCenter.defaultCenter addObserver:self selector:@selector(keyboardDidShow:) name:UIKeyboardDidShowNotification object:nil];
+        [NSNotificationCenter.defaultCenter addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
         [NSNotificationCenter.defaultCenter addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
     }
     
@@ -43,13 +45,34 @@
     [NSNotificationCenter.defaultCenter removeObserver:self];
 }
 
-- (void)keyboardDidShow:(NSNotification *)notification
+- (void)keyboardWillShow:(NSNotification *)notification
 {
-    NSValue *keyboardFrameAsValue = [notification.userInfo objectForKey:UIKeyboardFrameBeginUserInfoKey];
+    NSValue *keyboardFrameAsValue = [notification.userInfo objectForKey:UIKeyboardFrameEndUserInfoKey];
     CGRect keyboardFrame = [self.viewController.view convertRect:keyboardFrameAsValue.CGRectValue toView:nil];
-    CGRect frame = self.tableView.frame;
-    frame.size.height = self.viewController.view.frame.size.height - keyboardFrame.size.height;
-    self.tableView.frame = frame;
+    NSTimeInterval animationDuration;
+    [[notification.userInfo objectForKey:UIKeyboardAnimationDurationUserInfoKey] getValue:&animationDuration];
+    UIViewAnimationCurve animationCurve;
+    [[notification.userInfo objectForKey:UIKeyboardAnimationCurveUserInfoKey] getValue:&animationCurve];
+    UIViewAnimationOptions animationOptions;
+    switch (animationCurve) {
+        case UIViewAnimationCurveEaseInOut:
+            animationOptions = UIViewAnimationOptionCurveEaseInOut;
+        case UIViewAnimationCurveEaseIn:
+            animationOptions = UIViewAnimationOptionCurveEaseIn;
+        case UIViewAnimationCurveEaseOut:
+            animationOptions = UIViewAnimationOptionCurveEaseOut;
+        case UIViewAnimationCurveLinear:
+            animationOptions = UIViewAnimationOptionCurveLinear;
+        // no default
+    }
+
+    self.tableView.hidden = FALSE;
+    [UIView animateWithDuration:animationDuration delay:0.0 options:animationOptions animations:^{
+        CGRect frame = self.tableView.frame;
+        frame.size.height -= keyboardFrame.size.height;
+        self.tableView.frame = frame;
+        self.tableView.alpha = 1;
+    } completion:NULL];
 }
 
 - (void)keyboardWillHide:(NSNotification *)notification

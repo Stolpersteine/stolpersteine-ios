4b8b1bf87de9b18a4dc1575b8ab246651d37722b
Claus Höfele <choefele@ebay.com>
2013-02-01 15:07:22 +0100

    Added GA SDK 2.0beta4

--- Stolpersteine/AppDelegate.m
+++ Stolpersteine/AppDelegate.m
@@ -9,6 +9,7 @@
 #import "AppDelegate.h"
 
 #import "StolpersteineNetworkService.h"
+#import "GAI.h"
 
 #ifdef DEBUG
 @interface NSURLRequest (HTTPS)
@@ -16,18 +17,26 @@
 @end
 #endif
 
-static NSString * const BASE_URL = @"https://stolpersteine-optionu.rhcloud.com/api/";
+static NSString * const API_URL = @"https://stolpersteine-optionu.rhcloud.com/api/";
+static NSString * const GOOGLE_ANALYTICS_ID = @"UA-38166041-1";
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    NSURL *url = [NSURL URLWithString:BASE_URL];
+    NSURL *url = [NSURL URLWithString:API_URL];
     self.networkService = [[StolpersteineNetworkService alloc] initWithURL:url clientUser:nil clientPassword:nil];
 #ifdef DEBUG
     // This allows invalid certificates so that proxies can decrypt the traffic.
     [NSURLRequest setAllowsAnyHTTPSCertificate:YES forHost:url.host];
 #endif
+    
+    // Google Analytics
+//    GAI.sharedInstance.trackUncaughtExceptions = YES;
+//    GAI.sharedInstance.dispatchInterval = 60;
+//    GAI.sharedInstance.debug = YES;
+//    id<GAITracker> tracker = [GAI.sharedInstance trackerWithTrackingId:GOOGLE_ANALYTICS_ID];
+//    [tracker sendView:@"MapView"];
     
     return YES;
 }

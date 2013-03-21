79a2e604ff8a4c4cff57bbebf410e5b018189f34
Claus Hoefele <claus@claushoefele.com>
2013-03-21 19:34:45 +0100

    Optimize MapClusteringControllerFindClosestAnnotation

--- Stolpersteine/iPhone/MapClusteringControllerUtils.m
+++ Stolpersteine/iPhone/MapClusteringControllerUtils.m
@@ -10,19 +10,16 @@
 
 id<MKAnnotation> MapClusteringControllerFindClosestAnnotation(NSSet *annotations, MKMapPoint mapPoint)
 {
-    NSArray *sortedAnnotations = [annotations.allObjects sortedArrayUsingComparator:^NSComparisonResult(id obj1, id obj2) {
-        MKMapPoint mapPoint1 = MKMapPointForCoordinate(((id<MKAnnotation>)obj1).coordinate);
-        MKMapPoint mapPoint2 = MKMapPointForCoordinate(((id<MKAnnotation>)obj2).coordinate);
-        
-        CLLocationDistance distance1 = MKMetersBetweenMapPoints(mapPoint1, mapPoint);
-        CLLocationDistance distance2 = MKMetersBetweenMapPoints(mapPoint2, mapPoint);
-        
-        if (distance1 < distance2) {
-            return NSOrderedAscending;
-        } else {
-            return NSOrderedDescending;
+    id<MKAnnotation> closestAnnotation;
+    CLLocationDistance closestDistance = DBL_MAX;
+    for (id<MKAnnotation> annotation in annotations) {
+        MKMapPoint annotationAsMapPoint = MKMapPointForCoordinate(annotation.coordinate);
+        CLLocationDistance distance = MKMetersBetweenMapPoints(mapPoint, annotationAsMapPoint);
+        if (distance < closestDistance) {
+            closestDistance = distance;
+            closestAnnotation = annotation;
         }
-    }];
+    }
     
-    return sortedAnnotations.count > 0 ? [sortedAnnotations objectAtIndex:0] : nil;
+    return closestAnnotation;
 }

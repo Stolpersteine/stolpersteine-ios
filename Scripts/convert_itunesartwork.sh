6b4a57f8f4882ca22e23df010a54a2c666065010
Claus Höfele <claus@claushoefele.com>
2013-04-16 11:25:58 +0200

    Updated icons and added script to convert app icons

---
+++ Scripts/convert_itunesartwork.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+convert iTunesArtwork\@2x.png -resize 72 Stolpersteine/Resources/App\ Icons/Icon-72.png 
+convert iTunesArtwork\@2x.png -resize 144 Stolpersteine/Resources/App\ Icons/Icon-72\@2x.png 
+convert iTunesArtwork\@2x.png -resize 50 Stolpersteine/Resources/App\ Icons/Icon-Small-50.png 
+convert iTunesArtwork\@2x.png -resize 100 Stolpersteine/Resources/App\ Icons/Icon-Small-50\@2x.png 
+convert iTunesArtwork\@2x.png -resize 29 Stolpersteine/Resources/App\ Icons/Icon-Small.png 
+convert iTunesArtwork\@2x.png -resize 58 Stolpersteine/Resources/App\ Icons/Icon-Small\@2x.png 
+convert iTunesArtwork\@2x.png -resize 57 Stolpersteine/Resources/App\ Icons/Icon.png 
+convert iTunesArtwork\@2x.png -resize 114 Stolpersteine/Resources/App\ Icons/Icon\@2x.png 

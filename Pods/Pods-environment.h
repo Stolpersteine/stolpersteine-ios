8710f3c36878aa219d64f77ed09927ed2d87e7b8
Claus Höfele <claus@claushoefele.com>
2013-06-18 23:57:41 +0200

    Updated AFNetworking to 1.3.1

--- Pods/Pods-environment.h
+++ Pods/Pods-environment.h
@@ -10,7 +10,7 @@
 #define COCOAPODS_POD_AVAILABLE_AFNetworking
 #define COCOAPODS_VERSION_MAJOR_AFNetworking 1
 #define COCOAPODS_VERSION_MINOR_AFNetworking 3
-#define COCOAPODS_VERSION_PATCH_AFNetworking 0
+#define COCOAPODS_VERSION_PATCH_AFNetworking 1
 
 // AGWindowView
 #define COCOAPODS_POD_AVAILABLE_AGWindowView

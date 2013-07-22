bd2a760222fde86c8bf279913f40262ea9ccffb1
Claus Höfele <claus@claushoefele.com>
2013-07-22 14:13:15 +0200

    Commented out debug code

--- Stolpersteine/iPhone/StolpersteinDetailViewController.m
+++ Stolpersteine/iPhone/StolpersteinDetailViewController.m
@@ -38,13 +38,15 @@
     
     self.title = NSLocalizedString(@"StolpersteinDetailViewController.title", nil);
     
-    if (self.stolperstein == nil) {
-        self.stolperstein = [[Stolperstein alloc] init];
-    }
-    NSString *urlString0 = @"http://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Stolperstein_Robert_Remak%2C_Berlin_01.jpg/640px-Stolperstein_Robert_Remak%2C_Berlin_01.jpg";
-    NSString *urlString1 = @"http://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Wismar_St._Marien_2008-06-10.jpg/450px-Wismar_St._Marien_2008-06-10.jpg";
-    NSString *urlString2 = @"http://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Stolperstein_Elberfelder_Str_20_%28Moab%29_Margarete_Alexander.jpg/300px-Stolperstein_Elberfelder_Str_20_%28Moab%29_Margarete_Alexander.jpg";
-    self.stolperstein.imageURLStrings = @[urlString0, urlString1, urlString2];
+//    if (self.stolperstein == nil) {
+//        self.stolperstein = [[Stolperstein alloc] init];
+//    }
+//    NSString *urlString0 = @"http://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Stolperstein_Robert_Remak%2C_Berlin_01.jpg/640px-Stolperstein_Robert_Remak%2C_Berlin_01.jpg";
+//    NSString *urlString1 = @"http://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Wismar_St._Marien_2008-06-10.jpg/450px-Wismar_St._Marien_2008-06-10.jpg";
+//    NSString *urlString2 = @"http://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Stolperstein_Elberfelder_Str_20_%28Moab%29_Margarete_Alexander.jpg/300px-Stolperstein_Elberfelder_Str_20_%28Moab%29_Margarete_Alexander.jpg";
+//    self.stolperstein.imageURLStrings = @[urlString0, urlString1, urlString2];
+    
+//    self.stolperstein.personBiographyURLString = @"http://www.bochum.de/C12571A3001D56CE/vwContentByKey/W287J9EG297BOLDDE/$FILE/016_018_Simons_Ellen_Sophie_und_Hermann.pdf";
     
     // Name and address
     Stolperstein *stolperstein = self.stolperstein;

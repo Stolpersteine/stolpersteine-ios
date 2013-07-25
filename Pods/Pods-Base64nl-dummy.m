ebbcb7eb3c1def9af3a46ac9d26f35014f933a62
Claus Höfele <claus@claushoefele.com>
2013-07-25 17:19:43 +0200

    Updated to cocoa pods 22.3

---
+++ Pods/Pods-Base64nl-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Base64nl : NSObject
+@end
+@implementation PodsDummy_Pods_Base64nl
+@end
---
+++ Pods/Pods-Stolpersteine Unit Tests-AGWindowView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Stolpersteine_Unit_Tests_AGWindowView : NSObject
+@end
+@implementation PodsDummy_Pods_Stolpersteine_Unit_Tests_AGWindowView
+@end

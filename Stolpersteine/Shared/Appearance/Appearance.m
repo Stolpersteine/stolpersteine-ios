acffa6a7aa4cb03dc60eb180f34444898f75f5ae
Claus Höfele <claus@claushoefele.com>
2013-09-19 22:18:29 +0200

    Updated to newer attribute names

--- Stolpersteine/Shared/Appearance/Appearance.m
+++ Stolpersteine/Shared/Appearance/Appearance.m
@@ -38,10 +38,11 @@
     [navigationBar setBackgroundImage:[UIImage imageNamed:@"navigation-bar-portrait.png"] forBarMetrics:UIBarMetricsDefault];
     [navigationBar setBackgroundImage:[UIImage imageNamed:@"navigation-bar-landscape.png"] forBarMetrics:UIBarMetricsLandscapePhone];
     UIColor *grayColor = [UIColor colorWithRed:115.0f/255.0f green:120.0f/255.0f blue:128.0f/255.0f alpha:1.0f];
-    UIColor *lightGrayColor = [UIColor colorWithRed:239.0f/255.0f green:230.0f/255.0f blue:234.0f/255.0f alpha:1.0f];
-    NSDictionary *navigationBarTitleTextAttributes = @{ UITextAttributeTextColor: grayColor,
-                                                        UITextAttributeTextShadowColor: lightGrayColor,
-                                                        UITextAttributeTextShadowOffset: [NSValue valueWithUIOffset:UIOffsetMake(0, 1)] };
+    NSShadow *shadow = [[NSShadow alloc] init];
+    shadow.shadowColor = [UIColor colorWithRed:239.0f/255.0f green:230.0f/255.0f blue:234.0f/255.0f alpha:1.0f];
+    shadow.shadowOffset = CGSizeMake(0, 1);
+    NSDictionary *navigationBarTitleTextAttributes = @{ NSForegroundColorAttributeName: grayColor,
+                                                        NSShadowAttributeName: shadow };
     navigationBar.titleTextAttributes = navigationBarTitleTextAttributes;
     navigationBar.tintColor = [UIColor colorWithRed:143.0f/255.0f green:147.0f/255.0f blue:155.0f/255.0f alpha:1.0f];;
 }

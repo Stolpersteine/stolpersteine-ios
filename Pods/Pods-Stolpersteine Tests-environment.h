57619cc4b1c2e93059ffc80bd7e5cbd34b8ffbda
Claus Höfele <claus@claushoefele.com>
2013-10-26 22:28:24 +0200

    Updated cocoapods and installed GA SDK 3.0.2

--- Pods/Pods-Stolpersteine Tests-environment.h
+++ Pods/Pods-Stolpersteine Tests-environment.h
@@ -16,7 +16,7 @@
 #define COCOAPODS_POD_AVAILABLE_GoogleAnalytics_iOS_SDK
 #define COCOAPODS_VERSION_MAJOR_GoogleAnalytics_iOS_SDK 3
 #define COCOAPODS_VERSION_MINOR_GoogleAnalytics_iOS_SDK 0
-#define COCOAPODS_VERSION_PATCH_GoogleAnalytics_iOS_SDK 1
+#define COCOAPODS_VERSION_PATCH_GoogleAnalytics_iOS_SDK 2
 
 // OCMock
 #define COCOAPODS_POD_AVAILABLE_OCMock

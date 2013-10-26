7ca136e679fa7da5d2e45ea6e260caa89475b50e
Claus Höfele <claus@claushoefele.com>
2013-10-26 20:56:23 +0200

    Added dark info button and photo for Gunter Demnig

---
+++ Stolpersteine/iPhone/InfoViewController.h
@@ -0,0 +1,15 @@
+//
+//  InfoViewController.h
+//  Stolpersteine
+//
+//  Created by Claus on 26.10.13.
+//  Copyright (c) 2013 Option-U Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfoViewController : UITableViewController
+
+- (IBAction)close:(UIBarButtonItem *)sender;
+
+@end
---
+++ Stolpersteine/iPhone/InfoViewController.m
@@ -0,0 +1,43 @@
+//
+//  InfoViewController.m
+//  Stolpersteine
+//
+//  Created by Claus on 26.10.13.
+//  Copyright (c) 2013 Option-U Software. All rights reserved.
+//
+
+#import "InfoViewController.h"
+
+@interface InfoViewController ()
+
+@end
+
+@implementation InfoViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)close:(UIBarButtonItem *)sender
+{
+    [self dismissViewControllerAnimated:YES completion:NULL];
+}
+
+@end

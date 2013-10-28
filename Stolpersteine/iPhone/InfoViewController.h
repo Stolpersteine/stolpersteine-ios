81f3c89dceb5f28df4fe71bd45632236a7624951
Claus Höfele <claus@claushoefele.com>
2013-10-28 22:42:35 +0100

    WIP info screen

--- Stolpersteine/iPhone/InfoViewController.h
+++ Stolpersteine/iPhone/InfoViewController.h
@@ -10,9 +10,24 @@
 
 @interface InfoViewController : UITableViewController
 
-@property (weak, nonatomic) IBOutlet UILabel *stolpersteineTextLabel;
+@property (weak, nonatomic) IBOutlet UILabel *stolpersteineLabel;
 @property (weak, nonatomic) IBOutlet UIButton *stolpersteineInfoButton;
 @property (weak, nonatomic) IBOutlet UIButton *artistInfoButton;
+
+@property (weak, nonatomic) IBOutlet UILabel *aboutLabel;
+@property (weak, nonatomic) IBOutlet UIButton *ratingButton;
+@property (weak, nonatomic) IBOutlet UIButton *recommendButton;
+
+@property (weak, nonatomic) IBOutlet UILabel *sourceLabel;
+@property (weak, nonatomic) IBOutlet UIButton *kssButton;
+@property (weak, nonatomic) IBOutlet UIButton *wikipediaButton;
+
+@property (weak, nonatomic) IBOutlet UILabel *acknowledgementsLabel;
+@property (weak, nonatomic) IBOutlet UIButton *contactButton;
+@property (weak, nonatomic) IBOutlet UIButton *gitHubButton;
+@property (weak, nonatomic) IBOutlet UIButton *librariesButton;
+
+@property (weak, nonatomic) IBOutlet UILabel *legalLabel;
 
 - (IBAction)close:(UIBarButtonItem *)sender;
 
--- Stolpersteine/iPhone/InfoViewController.m
+++ Stolpersteine/iPhone/InfoViewController.m
@@ -21,9 +21,28 @@
 {
     [super viewDidLoad];
     
-    self.stolpersteineTextLabel.text = @"Stolpersteine sind kleine Gedenktafeln im Straßenpflaster zur Erinnerung an die Opfer des Nationalsozialismus. Mehr als 40.000 davon hat der Künstler Gunter Demnig mittlerweile in ganz Europa verlegt. Mit dieser App haben Sie schnell und einfach Zugriff auf Orte und Adressen der rund 5.000 Stolpersteinen in Berlin.";
+	// Stolpersteine
+    self.stolpersteineLabel.text = @"Stolpersteine sind kleine Gedenktafeln im Straßenpflaster zur Erinnerung an die Opfer des Nationalsozialismus. Mehr als 40.000 davon hat der Künstler Gunter Demnig mittlerweile in ganz Europa verlegt. Mit dieser App haben Sie schnell und einfach Zugriff auf Orte und Adressen der rund 5.000 Stolpersteine in Berlin.";
     [self.stolpersteineInfoButton setTitle:@"Stolpersteine auf Wikipedia" forState:UIControlStateNormal];
     [self.artistInfoButton setTitle:@"Gunter Demnigs Webseite" forState:UIControlStateNormal];
+	
+	// About
+	self.aboutLabel.text = @"Gefällt Ihnen diese App? Über Ihre Bewertung oder eine Weiterempfehlung würden wir uns freuen.";
+	[self.ratingButton setTitle:@"Im App Store bewerten" forState:UIControlStateNormal];
+	[self.recommendButton setTitle:@"An Freunde weiterempfehlen" forState:UIControlStateNormal];
+	
+	// Sources
+	self.sourceLabel.text = @"Wir bedanken uns bei den folgenden Organisationen, die uns die Daten für diese App zur Verfügung gestellt haben.";
+	[self.kssButton setTitle:@"Koordinierungsstelle Stolpersteine Berlin" forState:UIControlStateNormal];
+	[self.wikipediaButton setTitle:@"Wikipedia" forState:UIControlStateNormal];
+	
+	// Acknowledgements
+	self.acknowledgementsLabel.text = @"Diese App ist quelloffen und jeder kann mitmachen. Beiträge von Claus Höfele, Hendrik Spree und Rachel Höfele";
+	[self.contactButton setTitle:@"Schreiben Sie uns eine E-Mail" forState:UIControlStateNormal];
+	[self.gitHubButton setTitle:@"Stolpersteine App auf GitHub" forState:UIControlStateNormal];
+	
+	// Legal
+	self.legalLabel.text = @"Claus Höfele\nCalvinstr. 20b\n10557 Berlin";
 }
 
 - (void)viewDidAppear:(BOOL)animated
@@ -52,12 +71,8 @@
     if (section == 0) {
         title = @"Stolpersteine";
     } else if (section == 1) {
-        title = @"Über die App";
+        title = @"Über diese App";
     } else if (section == 2) {
-        title = @"Quellenangaben";
-    } else if (section == 3) {
-        title = @"Danksagungen";
-    } else if (section == 4) {
         title = @"Impressum";
     } else {
         title = [super tableView:tableView titleForHeaderInSection:section];

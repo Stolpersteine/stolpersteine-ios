11570e7a288bf15ccd309d50e732748659584eb9
Claus Höfele <claus@claushoefele.com>
2014-01-18 23:14:51 +0100

    First version of project configured to support both Berlin and Bochum apps

--- Podfile
+++ Podfile
@@ -1,5 +1,7 @@
 platform :ios, '7.0'
 
+link_with 'Stolpersteine Berlin', 'Stolpersteine Bochum'
+
 pod 'CCHMapClusterController'
 pod 'CCHMapsActivity'
 pod 'AFNetworking', '~> 1.3.3'
---
+++ Stolpersteine Berlin/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Stolpersteine Berlin
+//
+//  Created by Claus Höfele on 18.01.14.
+//  Copyright (c) 2014 Option-U Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

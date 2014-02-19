503c2cb337c2d0a7ee69a8a386d1da9b3746701f
Claus Höfele <claus@claushoefele.com>
2014-02-19 22:53:11 +0100

    Removed obsolete code

--- Stolpersteine/iPhone/StolpersteinAnnotationView.m
+++ Stolpersteine/iPhone/StolpersteinAnnotationView.m
@@ -26,30 +26,6 @@
 #import "StolpersteinAnnotationView.h"
 
 #define FOREGROUND_COLOR [UIColor colorWithWhite:(244.0 / 255.0) alpha:1.0]
-#define BACKGROUND_COLOR [UIColor colorWithRed:(254.0 / 255.0) green:(148.0 / 255.0) blue:(40.0 / 255.0) alpha:0.95]
-
-static inline CGPoint TBRectCenter(CGRect rect)
-{
-    return CGPointMake(CGRectGetMidX(rect), CGRectGetMidY(rect));
-}
-
-static inline CGRect TBCenterRectRounded(CGRect rect, CGPoint center)
-{
-    CGRect r = CGRectMake(roundf(center.x - rect.size.width/2.0),
-                          roundf(center.y - rect.size.height/2.0),
-                          roundf(rect.size.width),
-                          roundf(rect.size.height));
-    return r;
-}
-
-static CGFloat const TBScaleFactorAlpha = 0.3;
-static CGFloat const TBScaleFactorBeta = 0.4;
-
-static inline CGFloat TBScaledValueForValue(CGFloat value)
-{
-    return 1.0 / (1.0 + expf(-1 * TBScaleFactorAlpha * powf(value, TBScaleFactorBeta)));
-}
-
 @interface StolpersteinAnnotationView ()
 
 @property (strong, nonatomic) UILabel *countLabel;
@@ -66,29 +42,26 @@ static inline CGFloat TBScaledValueForValue(CGFloat value)
         [self setUpLabel];
         _oneLocation = NO;
         self.count = 1;
-        
-//        for (int i = 0; i < 10000; i++) {
-//            NSLog(@"%tu: %f", i, 44 * TBScaledValueForValue(i));
-//        }
     }
     return self;
 }
 
 - (void)setUpLabel
 {
-    _countLabel = [[UILabel alloc] initWithFrame:self.bounds];
-    _countLabel.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-    _countLabel.textAlignment = NSTextAlignmentCenter;
-    _countLabel.backgroundColor = [UIColor clearColor];
-    _countLabel.textColor = FOREGROUND_COLOR;
-    _countLabel.textAlignment = NSTextAlignmentCenter;
-    _countLabel.adjustsFontSizeToFitWidth = YES;
-    _countLabel.minimumScaleFactor = 2;
-    _countLabel.numberOfLines = 1;
-    _countLabel.font = [UIFont boldSystemFontOfSize:12];
-    _countLabel.baselineAdjustment = UIBaselineAdjustmentAlignCenters;
+    UILabel *countLabel = [[UILabel alloc] initWithFrame:self.bounds];
+    countLabel.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    countLabel.textAlignment = NSTextAlignmentCenter;
+    countLabel.backgroundColor = [UIColor clearColor];
+    countLabel.textColor = FOREGROUND_COLOR;
+    countLabel.textAlignment = NSTextAlignmentCenter;
+    countLabel.adjustsFontSizeToFitWidth = YES;
+    countLabel.minimumScaleFactor = 2;
+    countLabel.numberOfLines = 1;
+    countLabel.font = [UIFont boldSystemFontOfSize:12];
+    countLabel.baselineAdjustment = UIBaselineAdjustmentAlignCenters;
+    self.countLabel = countLabel;
     
-    [self addSubview:_countLabel];
+    [self addSubview:countLabel];
 }
 
 - (void)setOneLocation:(BOOL)oneLocation
